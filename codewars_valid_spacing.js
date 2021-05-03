ce614ad89247d29be134dc0d5aaec1dc07c068ec
Crew Spence <csspence123@gmail.com>
2021-05-03 06:31:02 -0500

    Wrote working solution.

--- codewars_valid_spacing.js
+++ codewars_valid_spacing.js
@@ -18,6 +18,20 @@ For this kata, the definition of valid spacing is one space between words, and n
 Note - there will be no punctuation or digits in the input string, only letters.
 */
 
-function validSpacing(s) {
-  // write your code here
+const validSpacing = (s) => {
+  let prevSpace = false;
+  if(s[0] === ' ' || s[s.length - 1] === ' ') {
+    return false;
+  }
+  for(let i = 0; i < s.length; i++) {
+    if(s[i] === ' ' && prevSpace === true) {
+      return false;
+    } else if(s[i] === ' ') {
+      prevSpace = true;
+    } else {
+      prevSpace = false;
+    }
+  }
+
+  return true;
 }
